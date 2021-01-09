50e751c68e42c3b352427a9c416e7ab6b315691a
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-01-09 23:51:00 +0800

    wip: yomo noise web demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NoiseProvider, useNoise } from "./NoiseProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NoiseProvider>
+      <div className="App">
+        <header className="App-header">
+          <NoiseDisplay />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </NoiseProvider>
   );
+}
+
+function NoiseDisplay() {
+  const x = useNoise();
+
+  return <pre>noise: [ {JSON.stringify(x, null, 2)} ]</pre>;
 }
 
 export default App;
