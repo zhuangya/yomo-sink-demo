830b8c9544a0c61ad5c57827fc7c14f83e978319
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-01-10 03:10:24 +0800

    draw line chart with react-charts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
+import { useMemo } from "react";
 import { NoiseProvider, useNoise } from "./NoiseProvider";
 import { useObservableState } from "observable-hooks";
+import { Chart, curveBundle } from "react-charts";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
 function App() {
   return (
     <NoiseProvider>
-      <div className="App">
-        <header className="App-header">
-          <NoiseDisplay />
-        </header>
-      </div>
+      <NoiseDisplay />
     </NoiseProvider>
   );
 }
 
 function NoiseDisplay() {
   const noise$ = useNoise();
+  const noiseStream = useObservableState(noise$);
+  const series = useMemo(() => ({ showPoints: false, curve: curveBundle }), []);
+  const axes = useMemo(
+    () => [
+      { primary: true, type: "time", show: true, position: "bottom" },
+      { type: "linear", position: "left", hardMin: 0, hardMax: 180 },
+    ],
+    []
+  );
 
-  const x = useObservableState(noise$);
+  const data = useMemo(() => noiseStream || [], [noiseStream]);
 
   return (
-    <pre>
-    {
-      JSON.stringify(x, null, 2)
-    }
-    </pre>
+    <Wrapper>
+      <h1>YoMo</h1>
+
+      <div style={{ width: "800px", height: "600px" }}>
+        <Chart data={data} series={series} axes={axes} />
+      </div>
+    </Wrapper>
   );
 }
 
--- src/NoiseProvider.js
+++ src/NoiseProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
 import { EMPTY, fromEvent } from "rxjs";
-import { scan } from "rxjs/operators";
+import { map, scan } from "rxjs/operators";
 import io from "socket.io-client";
 
 const NoiseContext = createContext({});
@@ -10,11 +10,29 @@ export function NoiseProvider({ children }) {
   const [noise$, setNoise$] = useState(EMPTY);
 
   useEffect(() => {
-    const socket = io("https://yomo.cel-la.store");
+    const socket = io("https://yomo.cel-la.store", {
+      transports: ["websocket"],
+    });
 
     setNoise$(
       fromEvent(socket, "receive_sink").pipe(
-        scan((acc, one) => [...acc, one.toFixed(1)].slice(-10), []),
+        scan(
+          (acc, one, index) =>
+            [
+              ...acc,
+              {
+                primary: index,
+                secondary: one,
+              },
+            ].slice(-10),
+          []
+        ),
+        map((data) => [
+          {
+            label: "noise",
+            data,
+          },
+        ])
       )
     );
 
