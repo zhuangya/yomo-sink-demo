ad1d5b09354041080a0e2e0f17d42d391f6e33b2
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-01-12 01:00:12 +0800

    better animation

--- src/NoiseChart.js
+++ src/NoiseChart.js
@@ -1,8 +1,20 @@
 import { Line } from "@nivo/line";
 import { useObservableState } from "observable-hooks";
+import styled from "styled-components";
 
-import { useNoise } from "./NoiseProvider";
+import { useNoise, useLastReading } from "./NoiseProvider";
 
+const Num = styled.span`
+  font-variant-numeric: tabular-nums;
+`;
+
+const Wrapper = styled.section`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 function foo(list = []) {
   return [
     {
@@ -16,50 +28,64 @@ export default function NoiseChart() {
   const noise$ = useNoise();
   const noiseStream = useObservableState(noise$);
 
+  const lastReading$ = useLastReading();
+  const lr = useObservableState(lastReading$);
+
   if (typeof noiseStream === "undefined") {
     return <pre>'loading'</pre>;
   }
 
   return (
-    <Line
-      data={foo(noiseStream)}
-      width={800}
-      height={600}
-      animate={true}
-      motionConfig='slow'
-      margin={{
-        top: 30,
-        right: 80,
-        bottom: 30,
-        left: 50,
-      }}
-      layers={["lines", "axes", "grid"]}
-      xScale={{
-        type: "point",
-        min: "auto",
-        max: "auto",
-      }}
-      yScale={{
-        type: "linear",
-        stacked: false,
-        min: 0,
-        max: 120,
-      }}
-      yFormat=">-.2"
-      enableGridX={false}
-      axisLeft={{
-        orient: "left",
-        tickSize: 0,
-        tickPadding: 6,
-        tickRotation: 30,
-      }}
-      axisBottom={{
-        orient: "bottom",
-        tickSize: 0,
-        tickPadding: 6,
-        tickRotation: 0,
-        format: () => null,
-      }}
-    />
+    <Wrapper>
+      <section>实时噪音分贝值： <Num>{lr.toFixed(1)}</Num></section>
+
+      <div style={{ width: "800px", height: "600px" }}>
+        <Line
+          data={foo(noiseStream)}
+          width={800}
+          height={600}
+          animate={true}
+          motionConfig={{
+            mass: 2,
+            tension: 230,
+            friction: 10,
+            clamp: true,
+          }}
+          margin={{
+            top: 30,
+            right: 80,
+            bottom: 30,
+            left: 50,
+          }}
+          curve="stepAfter"
+          layers={["lines", "axes", "grid"]}
+          xScale={{
+            type: "point",
+            min: "auto",
+            max: "auto",
+          }}
+          yScale={{
+            type: "linear",
+            stacked: false,
+            min: 20,
+            max: 100,
+          }}
+          enableGridX={false}
+          axisLeft={{
+            orient: "left",
+            tickSize: 0,
+            tickPadding: 6,
+            tickRotation: 30,
+          }}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 0,
+            tickPadding: 6,
+            tickRotation: 0,
+            format: () => null,
+          }}
+        />
+      </div>
+    </Wrapper>
   );
 }
--- src/NoiseProvider.js
+++ src/NoiseProvider.js
@@ -1,28 +1,21 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
-import { EMPTY, fromEvent } from "rxjs";
-import { tap, windowTime, defaultIfEmpty, last, share, map, scan, mergeAll } from "rxjs/operators";
+import { timer, EMPTY, fromEvent } from "rxjs";
+import { withLatestFrom, share, scan } from "rxjs/operators";
 import io from "socket.io-client";
 
 const NoiseContext = createContext({});
 
-function feedChart() {
+const clock$ = timer(20, 200);
+
+function feedChart(data$, size = 10, defaultResult = null) {
   return function (source) {
     return source.pipe(
-      windowTime(500),
-      map(win => win.pipe(
-        defaultIfEmpty(null),
-        last()
-      )),
-      mergeAll(),
+      withLatestFrom(data$, (_, x) => x),
       scan(
-        (acc, one) =>
-          [
-            ...acc,
-            one,
-          ].slice(-10),
-        Array(10).fill(null)
-      ),
+        (acc, one) => [...acc, one].slice(-size),
+        Array(size).fill(defaultResult)
+      )
     );
   };
 }
@@ -35,8 +28,8 @@ export function NoiseProvider({ children }) {
       transports: ["websocket"],
     });
 
-    const raw$ = fromEvent(socket, "receive_sink").pipe(share())
-    setRaw$(raw$)
+    const raw$ = fromEvent(socket, "receive_sink").pipe(share());
+    setRaw$(raw$);
 
     return () => {
       setRaw$(null);
@@ -45,7 +38,11 @@ export function NoiseProvider({ children }) {
   }, []);
 
   return (
-    <NoiseContext.Provider value={{ noise$: raw$.pipe(feedChart()), lastReading$: raw$ }}>{children}</NoiseContext.Provider>
+    <NoiseContext.Provider
+      value={{ noise$: clock$.pipe(feedChart(raw$)), lastReading$: raw$ }}
+    >
+      {children}
+    </NoiseContext.Provider>
   );
 }
 
